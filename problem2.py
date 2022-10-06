31a720c2bf35601a38755a4a4750458068ea8471
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-06 12:57:11 +0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,6 +1,4 @@
 def p2():
     inlist = input().split()
-    inlist2 = input().split()
-    inlist_all = inlist + inlist2
-    inlist_all.sort(reverse=True)
-    print(" ".join(inlist_all))
+    outlist = [i for i in inlist if i>=0]
+    print(" ".join(outlist))

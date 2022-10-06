ff3963e5f6a2642b8eaffb183b63af75fe891fa3
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-06 18:58:54 +0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,5 +1,5 @@
 def p4():
    posneg = [ int(i) for i in input().split()]
-   noneg = [i for i in posneg if i<0]
-   ans = len(noneg)
+   numneg = [i for i in posneg if i<0]
+   ans = len(numneg)
    print(ans)

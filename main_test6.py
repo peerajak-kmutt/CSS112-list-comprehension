d1409139264112497a6ddf87b1c2187e12dabe26
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-06 18:49:02 +0700

    Create main_test6.py

---
+++ main_test6.py
@@ -0,0 +1,15 @@
+from problem6 import p6
+import io
+
+
+def test_problem4(monkeypatch,capsys):  # or use "capfd" for fd-level
+    with open('infile6.txt') as fi:
+        lines_i = [line for line in fi]
+    str_in = "".join(lines_i)
+    with open('outfile6.txt') as fo:
+        lines_o = [line for line in fo]
+    str_out = "".join(lines_o)
+    monkeypatch.setattr('sys.stdin', io.StringIO(str_in))
+    p6() 
+    captured = capsys.readouterr()
+    assert captured.out == str_out
